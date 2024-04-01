feb745e810c60da99c767fe9e0cd6bb04d161736
meet <panchalmeetr97@gmail.com>
2024-04-01 11:48:58 +0530

    redux

--- cart/src/Components/Header.js
+++ cart/src/Components/Header.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 const Header = () => {
   return (
   <div>
@@ -11,10 +12,10 @@ const Header = () => {
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
+        <ShoppingCartIcon style={{ fontSize: "30px", float:"right", marginRight:'15px', color:"white",cursor:'pointer' }} />
     </Container>
+    
   </Navbar>
   </div>
   )
