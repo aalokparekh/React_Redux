38301e1786f49298aa248f3645dee990dbb6e77c
Aalok Parekh <156985863+aalokparekh@users.noreply.github.com>
2024-04-02 06:18:39 +0000

    cart

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import { Card } from '@mui/material';
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes> 
+        <Route path='/' element={<Card />}></Route>
+      </Routes>
     </div>
   );
 }
--- cart/src/Components/Header.js
+++ cart/src/Components/Header.js
@@ -4,20 +4,43 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Menu } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-  <div>
-  <Navbar bg="dark" data-bs-theme="dark">
-    <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-        </Nav>
-        <ShoppingCartIcon style={{ fontSize: "30px", float:"right", marginRight:'15px', color:"white",cursor:'pointer' }} />
-    </Container>
-    
-  </Navbar>
-  </div>
+    <div>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <ShoppingCartIcon style={{ fontSize: "30px", float: "right", marginRight: '15px', color: "white", cursor: 'pointer' }} />
+        </Container>
+
+        <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>>
+      </Navbar>
+    </div>
   )
 }
 
